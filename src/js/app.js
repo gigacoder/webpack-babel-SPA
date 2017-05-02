b96ac664dba69dca2f4198c706aa11d58d27f076
Parul Mishra <brunocoder@yahoo.com>
2017-05-02 12:13:42 -0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+import '../css/main.css';
+import '../css/input-elements.css';
+
+import '../css/bootstrap.min.css';
+
+import {secretParagraph, secretButton } from "./dom-loader"
+
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Hide the Secret';
+    } else {
+        secretButton.textContent = 'Show the Secret';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
